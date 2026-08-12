dd1912db17412d44d9581f39c440d4c3c6e23690
Fábio <54125542+ZHEIKE@users.noreply.github.com>
2026-08-11 22:09:28 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    primeira visita, para que tudo funcione sem internet depois disso.
    Os PDFs/manifestos ficam no IndexedDB, não aqui. */
 
-const CACHE_NAME = "tagviewer-cache-v4";
+const CACHE_NAME = "tagviewer-cache-v5";
 const ASSETS = [
   "./",
   "./index.html",
